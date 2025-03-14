84d6af03c96a9c5e761d467720fc39591a0cce3d
rr <endian.sign@gmail.com>
2025-03-14 18:09:06 +0300

    Define a batch script to install the vcpkg dependencies

---
+++ install-dependencies.bat
@@ -0,0 +1,9 @@
+cd vcpkg
+.\vcpkg install curl
+.\vcpkg install openssl
+.\vcpkg install boost
+.\vcpkg install libmicrohttpd
+.\vcpkg install sqlite3
+.\vcpkg install argtable2
+.\vcpkg integrate install
+cd ..
