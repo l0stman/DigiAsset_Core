663debb8a1ad3528554ebaddb94d8da4d0436c10
rr <endian.sign@gmail.com>
2025-03-15 03:13:40 +0300

    Configure also libjson-rpc-cpp

--- config.bat
+++ config.bat
@@ -1,3 +1,5 @@
 cd jsoncpp
 cmake -B build -DCMAKE_INSTALL_PREFIX=install -DCMAKE_INSTALL_INCLUDEDIR=jsoncpp
+cd ..\libjson-rpc-cpp
+cmake -B build -DCMAKE_INSTALL_PREFIX=install -DCMAKE_TOOLCHAIN_FILE=..\vcpkg\scripts\buildsystems\vcpkg.cmake -DCMAKE_PREFIX_PATH=..\jsoncpp\install
 cd ..
